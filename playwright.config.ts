c01842869a7cc429c3ad99969e8da85dfc27b47e
ShitalKendre <badeshital124@example.com>
2026-08-20 23:48:32 +0530

    updated config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,92 +1,76 @@
 import { defineConfig, devices } from '@playwright/test';
+
 import dotenv from 'dotenv';
-const ENV=process.env.ENV || "qa"
-console.log("Running tc on Enviornment",ENV);
-dotenv.config({path:`config/.env.${ENV}`});
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+//ENV=dev npx playwright test
+const ENV = process.env.ENV || "qa";
+console.log('Running tests on Environment: ', ENV);
+dotenv.config({ path: `config/.env.${ENV}` });
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests',
+  /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  workers: process.env.CI ? '100%' : undefined,
+
   reporter: [
-    ['html', { OutputFolder:'reports/html-report' ,open:'never' }],
-    ['allure-playwright',
-      {outputFolder:'allure-result',
-        subTitle:true,
-      }
-    ]
+    ["list"],
+    ["html", { outputFolder: "reports/html-report", open: "never" }],
+    ["allure-playwright", {
+      outputFolder: "allure-results",
+      suiteTitle: true,
+    }],
   ],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
   use: {
-   
     baseURL: process.env.BASE_URL,
-     headless: false,
-        browserName: 'chromium',
-        viewport: null,
-        ignoreHTTPSErrors: true,
-        
-    trace: 'on-first-retry'
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on-first-retry',
+    headless: !process.env.CI ? false : true,
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome']},
-    }
+      use: { ...devices['Desktop Chrome'] },
+    },
 
-    /*{
-       name: 'firefox',
-       use: { ...devices['Desktop Firefox'] },
-     },*/
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    /* {
-     name: 'webkit',
-       use: { ...devices['Desktop Safari'] },
-     },*/
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
-    /* {
-       name: 'Mobile Chrome',
-       use: { ...devices['Pixel 5'] },
-     },
-     {
-       name: 'Mobile Safari',
-       use: { ...devices['iPhone 12'] },
-     },*/
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
 
     /* Test against branded browsers. */
-   /**  {
-       name: 'Microsoft Edge',
-       use: { ...devices['Desktop Edge'], channel: 'msedge' },
-     },
-     {
-       name: 'Google Chrome',
-       use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-     },*/
-  ]
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
